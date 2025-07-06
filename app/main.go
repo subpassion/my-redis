88671341e38e5539311231c143c101e5a03577a1
algodump <dlyzno@gmail.com>
2025-07-06 15:41:15 +0200

    add pong message

--- app/main.go
+++ app/main.go
@@ -16,9 +16,11 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	con, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	con.Write([](byte)("+PONG\r\n"))
 }
